36de6dea4505d800a2d4aae4dae77c7a5c22a897
Edenilson João Pereira <52428137+ninojp@users.noreply.github.com>
2024-01-13 06:40:28 -0300

    Alura+ useState, Concluído

--- alura-mais-use-state/src/App.js
+++ alura-mais-use-state/src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 
-
 function App() {
   const [nome, setNome] = useState('Nino JP')
   // function trocaNome(){
@@ -17,6 +16,35 @@ function App() {
     }
     console.log(nome);
   }
+  //====================================================================================
+  // const [cep, setCep] = useState('');
+  const [endereco, setEndereco] = useState({});
+  function manipularEndereco(evento){
+    const cep =  evento.target.value
+    setEndereco({cep});
+    if(cep && cep.length === 8){
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(resposta => resposta.json())
+      // .then(dados => { setEndereco({
+      //     cep: cep,
+      //     rua: dados.logradouro,
+      //     bairro: dados.bairro,
+      //     cidade: dados.localidade,
+      //     estado: dados.uf
+      //   });
+      // Pode ser feito assim também
+      .then(dados => { setEndereco(enderecoAntigo => {
+          return {
+            ...enderecoAntigo,
+            rua: dados.logradouro,
+            bairro: dados.bairro,
+            cidade: dados.localidade,
+            estado: dados.uf
+          }
+        })
+      })
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -26,8 +54,21 @@ function App() {
         </h1>
       </header>
       <section className='corpo'>
-        <h2>{nome}</h2>
-        <button onClick={trocaNome}>Atualizar Nome</button>
+        <div className='trocar-nome'>
+          <h2>{nome}</h2>
+          <button onClick={trocaNome}>Atualizar Nome</button>
+        </div>
+        <div className='cep-digitado'>
+          <label >Digite o CEP</label>
+          <input placeholder='Digite o CEP' onChange={manipularEndereco}/>
+          <ul>
+            <li>CEP Digitado: {endereco.cep}</li>
+            <li>Rua: {endereco.rua}</li>
+            <li>Bairro: {endereco.bairro}</li>
+            <li>Cidade: {endereco.cidade}</li>
+            <li>Estado: {endereco.estado}</li>
+          </ul>
+        </div>
       </section>
     </div>
   );
