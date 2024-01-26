863a52fa19d07aedad3d3e5dcfb68f905e0b73d5
Edenilson João Pereira <52428137+ninojp@users.noreply.github.com>
2024-01-25 23:59:46 -0300

    Agora RODOU kkkk (depois de muitos erros)

---
+++ Curso_React_gerenciamento_de_estado_globais_com_Context_API/src/App.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+import './index.css';
+import Login from '/src/pages/Login';
+
+import './App.css'
+
+function App() {
+
+  return (
+        <Login />
+  )
+}
+
+export default App
---
+++ Curso_React_gerenciamento_de_estado_globais_com_Context_API/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+import './index.css'
+import Login from '/src/pages/Login/index.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <Login />
+  </React.StrictMode>,
+)
